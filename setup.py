a2b340fb5b204a55b4efc98b7815e0435b4fbe03
vhqr0 <zq_cmd@163.com>
2023-09-01 08:42:55 +0800

    setup: use compatible requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 
 import setuptools
 
+dependence = ['hy ~= 0.27.0', 'hyrule ~= 0.4.0', 'asyncrule ~= 1.0.0']
+
 setuptools.setup(
     name='structlib',
     version='1.0.0',
     author='vhqr',
     description='more flexible sturct library',
-    install_requires=['hy == 0.27.0', 'hyrule == 0.4.0', 'asyncrule == 1.0.0'],
+    install_requires=dependence,
     packages=['structlib'],
     package_data={
         'structlib': ['structlib/__init__.py', 'structlib/stream.py']
